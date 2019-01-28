3339fd66b4ab290b3fbfecae469900fb5da31351
active-object-6703 <nerajno@gmail.com>
2019-01-28 02:14:16 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  created_array = []
+  return created_array
+end
+
+def array_with_two_elements
+  crtd_array = [1, 2]
+end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+array[2]
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods(array)
+
+end
